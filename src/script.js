97a4effdc13bda49e49e5bd1ec3d993d015d2421
Ali Bazizi <abazizi@e2r11p4.1337.ma>
2022-02-16 16:46:27 +0100

    beta 1.3

---
+++ src/script.js
@@ -0,0 +1,30 @@
+
+const alternateStyles = document.querySelectorAll(".alternate-style");
+const alternateDark = document.querySelectorAll(".alternate-dark");
+function setActiveStyle(color)
+{
+    alternateStyles.forEach((style) => {
+        if(color === style.getAttribute("title"))
+        {
+            style.removeAttribute("disabled");
+        }
+        else
+        {
+            style.setAttribute("disabled","true");
+        }
+    })
+}
+
+function setDarkStyle(color)
+{
+    alternateDark.forEach((style) => {
+        if(color === style.getAttribute("title"))
+        {
+            style.removeAttribute("disabled");
+        }
+        else
+        {
+            style.setAttribute("disabled","true");
+        }
+    })
+}
